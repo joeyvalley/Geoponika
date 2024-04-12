1ca9d62fac815f36f28b152db948124725ef1995
Joseph Valle <josephrvalle@gmail.com>
2024-04-12 17:46:41 -0400

    max edits with original layout as an option'

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import Original from './components/Original';
 import White from './components/White';
 import Black from './components/Black';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<White />}></Route>
+      <Route path="/" element={<Original />}></Route>
+      <Route path="/white" element={<White />}></Route>
       <Route path="/black" element={<Black />}></Route>
     </Routes>
   );
